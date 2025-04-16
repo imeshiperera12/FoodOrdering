d4b1d31753472cb98bbc756108870ec633a36bba
Dilki687 <chamodidilki44@gmail.com>
2025-04-16 12:05:23 +0530

    added nodemon to payment-service

--- backend/payment-service/controllers/paymentController.js
+++ backend/payment-service/controllers/paymentController.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('../config/stripe');
 const Payment = require('../models/paymentModel');
 
 // Create a payment intent and store record
